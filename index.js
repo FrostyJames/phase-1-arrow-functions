e2f6e1f3d77e9d6e874b65a1f0a7ab9c4ee2a992
james <ivanlavan773@gmail.com>
2025-06-28 11:12:18 +0300

    Updated code for arrow functions

--- index.js
+++ index.js
@@ -8,3 +8,18 @@ const square = x => x * x;
 
 // Arrow function for add
 const add = (a, b) => a + b;
+
+// let a = 2000;
+// let b = 100;
+// console.log(divide(a, b))
+// output:20
+
+
+// let x =2;
+// console.log(square(x));
+// output:4
+
+// let a =3;
+// let b =4;
+// console.log(add(a, b));
+// output:7
